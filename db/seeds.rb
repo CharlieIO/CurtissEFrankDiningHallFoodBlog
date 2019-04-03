5456c4f840aff904952f6782304e3f3391e03ee8
Alex Diacovo <adiacovo@alexs-mbp-2.home>
2019-04-03 19:38:13 -0400

    added db/seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+BlogPost.delete_all
+
+BlogPost.create!(title: "Gross Spinach", description: "watery, and glowing green.", location: "Frank", category: "Dinner", rating: 1)
+BlogPost.create!(title: "Clam Chowder", description: "pretty good, feels like i'm back in Boston.", location: "Coop", category: "Lunch", rating: 4)
+BlogPost.create!(title: "Lovely Roast Chicken", description: "well seasoned, not dry, even better with bbq sauce.", location: "Library Cafe", category: "Dinner", rating: 5)
+BlogPost.create!(title: "Penne a la vodka", description: "ah, just like grandmothers!", location: "Coop", category: "Lunch", rating: 5)
+BlogPost.create!(title: "Sketchy Rice Pilaf", description: "dont try it, way too chunky and been sitting out for what seems like days.", location: "Coop", category: "Breakfast", rating: 0)

c93e10977eb52e922081152b37932f7b3106a220
Alex Diacovo <adiacovo@alexs-mbp-2.home>
2019-04-30 02:01:42 -0400

    back_to_top button

---
+++ app/assets/javascripts/blog_post.js
@@ -0,0 +1,61 @@
+"use strict";
+// window.$ = window.jQuery = require('jquery'); // not sure if you need this at all
+// window.Bootstrap = require('bootstrap');
+console.log()
+var blog_posts = (function() {
+    var comment = function(Arg) {
+      var event_detail = Arg.detail[2].responseText;
+      console.log(event_detail)
+      $("#comments ul").append(event_detail);
+      $("#commentbox")[0].value = "";
+    };
+
+    return {
+        init: function() {
+          $("form[data-remote]").on("ajax:success", comment);
+        }
+    };
+
+})();
+$(document).ready(function() {
+    var scrollup = (function() {
+        
+            
+            if (jQuery(this).scrollTop() < 100) {
+                jQuery(".scrollToTop").fadeOut(0);
+            }
+
+            else if ((document.readyState === 'complete') && (jQuery(this).scrollTop() >= 300)) {
+                jQuery(".scrollToTop").fadeIn(1000);
+                jQuery(".scrollToTop").click(function() {
+                    jQuery('html, body').animate({scrollTop : 0}, 50);
+                });
+                    // jQuery('html, body').animate({scrollTop : 0}, 500);
+                
+                // else {
+                //     return false;
+                // }
+            }
+        
+    })();
+});
+// $(scrollup);
+
+// console.log()
+// $(document).ready(function() {
+//     // $(window).scroll(function(){
+//     //     if ($(this).scrollTop() > 1) {
+//     //         $(".scrollToTop").fadeIn(1000)
+//     //     } 
+//     //     else {
+//     //         $(".scrollToTop").fadeOut(1000);
+//     //     }
+//     // });
+
+//     //Click event to scroll to top
+//     $(".scrollToTop").click(function(){
+//         $('html, body').animate({scrollTop : 0},500);
+//         return false;
+//     });
+// })();
+$(blog_posts.init);

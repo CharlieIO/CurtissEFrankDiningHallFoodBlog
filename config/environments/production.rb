01c5029bd927c83db2f7d04738e5d28a97a96882
Alex Diacovo <adiacovo@Alexs-MacBook-Pro-2.local>
2019-05-02 17:27:52 -0400

    compile backgorund image for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,6 +21,7 @@ Rails.application.configure do
   # Make sure images (background) compiled for heroku
   config.serve_static_assets = true
   config.assets.compile = true
+  config.assets.digest = true
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
@@ -31,7 +32,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  # config.assets.compile = false
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
